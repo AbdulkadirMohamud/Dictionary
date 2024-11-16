8addaedebec94d7d821bf5c3e86f25c0d81dd917
Abdulkadir <qansax30@hotmail.com>
2024-11-16 20:11:32 +0100

    Add project files.

---
+++ Dictionary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> names = new Dictionary<int, string>();
+            names.Add(1, "Abdulkadir");
+            names.Add(2, "Test");
+            names.Add(3, "Test");
+
+            //kKeyValuePair
+
+            Console.ReadLine();
+        }
+    }
+}
